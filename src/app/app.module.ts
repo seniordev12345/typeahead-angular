e2e4b945d3d8815e285514263325cb76e34c55e9
assasin-dev <leo.hhu5731@gmail.com>
2021-01-27 16:10:19 +0800

    completed typeahead by Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
+import { AppOptionComponent } from './app-option/app-option.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppOptionComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
